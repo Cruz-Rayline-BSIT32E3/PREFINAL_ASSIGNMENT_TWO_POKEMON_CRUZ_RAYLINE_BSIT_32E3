de293d1a83b6dc6b23c2268c88297f608ce72c3c
Cruz-Rayline-BSIT32E3 <raylinecruz182@gmail.com>
2024-06-08 23:45:10 +0800

    initial commit

--- PokemonApp/obj/Debug/net8.0/PokemonApp.AssemblyInfo.cs
+++ PokemonApp/obj/Debug/net8.0/PokemonApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff66725dfbeef41f55e3d238844a5ec8cf1648eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
